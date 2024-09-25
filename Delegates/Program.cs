345ff802f6b82ef183f00552299b7f15d6feaf0c
Jan Pool <jgpool@deltion.nl>
2024-09-25 14:37:00 +0200

    added my project files

---
+++ Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // instatiate the delegate
+        Callback handler = DelegateMethod;
+
+        // call the delegate
+        handler("Hello World");
+    }
+
+    public delegate void Callback(string message);
+
+    // create method for delegate.
+    public static void DelegateMethod(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+}
