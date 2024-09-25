f49b956ae7c0ae074db4b5c0570131552e2b693f
Jan Pool <jgpool@deltion.nl>
2024-09-25 14:55:32 +0200

    implemented the logic of gamenanager into Program.cs

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace DependencyInjection
+﻿using DependencyInjection;
+
+namespace DependencyInjection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameManager gm = new GameManager();
+
+            do
+            {
+                RoundResult result = gm.PlayGround();
+
+                if (result == RoundResult.Player1Win)
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                }
+                else if (result == RoundResult.Player2Win)
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                }
+                else
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+
+                Console.WriteLine("Play Again (Y/N)? ");
+            } while (Console.ReadLine().ToUpper() == "Y");
         }
     }
 }
